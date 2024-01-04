31ace41e7ff5ad8afcc5bab8e52f2ece4c72ceb7
Craig Fast <craigmaland@gmail.com>
2024-01-04 15:00:03 -0800

    Adding People works. Need work on adding presents

---
+++ lib/add_Birthday_Present.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:present_planner/my_main_page.dart';
+
+class AddBirthdayPersonPage extends StatefulWidget {
+  final List<String> arrayNames;
+  final List<String> arrayDates;
+  final List<double> arrayBudgets;
+
+  const AddBirthdayPersonPage({
+    Key? key,
+    required this.arrayNames,
+    required this.arrayDates,
+    required this.arrayBudgets,
+  }) : super(key: key);
+
+  @override
+  State<AddBirthdayPersonPage> createState() => _AddBirthdayPersonPageState();
+}
+
+class _AddBirthdayPersonPageState extends State<AddBirthdayPersonPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _budgetController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Add a New Birthday'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Please Fill Out All the Information',
+              ),
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: TextField(
+                  controller: _nameController,
+                  obscureText: false,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Add a Name',
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: TextField(
+                  controller: _budgetController,
+                  obscureText: false,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Add a Budget',
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.all(20),
+                child: TextField(
+                  controller: _dateController,
+                  obscureText: false,
+                  decoration: const InputDecoration(
+                    labelText: 'Add a Birthdate',
+                    filled: true,
+                    prefixIcon: Icon(Icons.calendar_today_outlined),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)
+                    ),
+                  ),
+                  readOnly: true,
+                  onTap: (){
+                    _selectDate();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                heroTag: "1",
+                child: Text('Save'),
+                onPressed: () => saveClicked(),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              FloatingActionButton(
+                heroTag: "2",
+                child: Text('Cancel'),
+                onPressed: () => cancelClicked(),
+              )
+            ]
+        )
+    );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now()
+    );
+
+    if (picked != null){
+      setState(() {
+        _dateController.text = picked.toString().split(" ")[0];
+        print(_dateController.text);
+      });
+    }
+  }
+  void cancelClicked() {
+    Navigator.pop(context);
+  }
+
+  void saveClicked() {
+    String newName = _nameController.text;
+    double newBudget = double.parse(_budgetController.text);
+
+    // Assuming you have a way to get the selected date as DateTime
+    DateTime selectedDate = DateTime.parse(_dateController.text);
+
+    // Update the arrays in MyMainPage
+    widget.arrayNames.add(newName);
+    widget.arrayDates.add(selectedDate.toString().split(" ")[0]);
+    widget.arrayBudgets.add(newBudget);
+
+    // Pass back the updated lists
+    Navigator.pop(context, {
+      'arrayNames': widget.arrayNames,
+      'arrayDates': widget.arrayDates,
+      'arrayBudgets': widget.arrayBudgets,
+    });
+  }
+}
--- lib/birthday_add_person.dart
+++ lib/birthday_add_person.dart
@@ -24,27 +24,15 @@ class _MyBirthdayPageState extends State<MyBirthdayPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Christmas Page',
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Christmas Page',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
